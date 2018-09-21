bdc74ad647be1d5aa4724ecc52a994eef490e6b4
Matthew Bonanni <MatthewBonanni@users.noreply.github.com>
2018-09-21 13:08:29 -0400

    Implemented new mobility visualization based on robotics system toolbox

--- arm_def.m
+++ arm_def.m
@@ -1,6 +1,7 @@
 ValkArm = robotics.RigidBodyTree;
 
 bLink1 = robotics.RigidBody('bLink1');
+bLink2 = robotics.RigidBody('bLink2');
 link1 = robotics.RigidBody('link1');
 link2 = robotics.RigidBody('link2');
 link3 = robotics.RigidBody('link3');
@@ -10,43 +11,37 @@ len1 = 0.3;
 len2 = 0.03;
 len3 = 0.3;
 
-bAct1 = robotics.Joint('bLink1', 'prismatic');
+bAct1 = robotics.Joint('bLink1', 'fixed');
+bAct2 = robotics.Joint('bLink2', 'prismatic');
 act1 = robotics.Joint('link1', 'revolute');
 act2 = robotics.Joint('link2', 'revolute');
 act3 = robotics.Joint('link3', 'revolute');
 actE = robotics.Joint('linkE', 'fixed'); % end effector
 
-bAct1.PositionLimits = [-0.5 0.5];
-act1.PositionLimits = [-pi pi];
-act2.PositionLimits = [-pi/2 pi/2];
-act3.PositionLimits = [-pi pi];
-
-dhparams = [0   0     0     0;
+dhparams = [0   pi/2  0     pi;
+            0   0     0     0;
             0   -pi/2 0     0;
             0   pi/2  0     0;
             0   0     len2  0;
             0   0     len3  0];
 
 setFixedTransform(bAct1, dhparams(1,:), 'dh');
-setFixedTransform(act1, dhparams(2,:), 'dh');
-setFixedTransform(act2, dhparams(3,:), 'dh');
-setFixedTransform(act3, dhparams(4,:), 'dh');
-setFixedTransform(actE, dhparams(5,:), 'dh');
+setFixedTransform(bAct2, dhparams(2,:), 'dh');
+setFixedTransform(act1, dhparams(3,:), 'dh');
+setFixedTransform(act2, dhparams(4,:), 'dh');
+setFixedTransform(act3, dhparams(5,:), 'dh');
+setFixedTransform(actE, dhparams(6,:), 'dh');
 
 bLink1.Joint = bAct1;
+bLink2.Joint = bAct2;
 link1.Joint = act1;
 link2.Joint = act2;
 link3.Joint = act3;
 linkE.Joint = actE;
 
 addBody(ValkArm, bLink1, 'base');
-addBody(ValkArm, link1, 'bLink1');
+addBody(ValkArm, bLink2, 'bLink1');
+addBody(ValkArm, link1, 'bLink2');
 addBody(ValkArm, link2, 'link1');
 addBody(ValkArm, link3, 'link2');
 addBody(ValkArm, linkE, 'link3');
-
-config = ValkArm.randomConfiguration;
-tform = getTransform(ValkArm, config, 'linkE', 'base');
-
-showdetails(ValkArm);
-show(ValkArm, config);
---
+++ mobility.m
@@ -0,0 +1,57 @@
+arm_def();
+
+bAct2.PositionLimits = [-0.5 0.5];
+act1.PositionLimits = [-pi pi];
+act2.PositionLimits = [-pi/2 pi/2];
+act3.PositionLimits = [-pi pi];
+
+resolution = 20;
+
+bAct2_pos = linspace(bAct2.PositionLimits(1), ...
+                     bAct2.PositionLimits(2), ...
+                     resolution);
+act1_pos = linspace(act1.PositionLimits(1), ...
+                    act1.PositionLimits(2), ...
+                    resolution);
+act2_pos = linspace(act2.PositionLimits(1), ...
+                    act2.PositionLimits(2), ...
+                    resolution);
+act3_pos = linspace(act3.PositionLimits(1), ...
+                    act3.PositionLimits(2), ...
+                    resolution);
+
+%config = ValkArm.randomConfiguration;
+config = struct('JointName', [], 'JointPosition', []);
+config(1).JointName = 'bLink2';
+config(2).JointName = 'link1';
+config(3).JointName = 'link2';
+config(4).JointName = 'link3';
+
+endpts = [];
+
+for i = 1:length(bAct2_pos)
+    config(1).JointPosition = bAct2_pos(i);
+    
+    for j = 1:length(act1_pos)
+        config(2).JointPosition = act1_pos(j);
+        
+        for k = 1:length(act2_pos)
+            config(3).JointPosition = act2_pos(k);
+            
+            % This joint only affects orientation of end effector -
+            % ignoring for now
+            config(4).JointPosition = act3_pos(1);
+            
+            % Calculate transform matrix
+            tform = getTransform(ValkArm, config, 'linkE', 'base');
+            
+            % Add endpoint to array
+            endpts = cat(1, endpts, tform2trvec(tform));
+        end
+    end
+end
+
+scatter3(endpts(:,1), endpts(:,2), endpts(:,3), 'k.')
+
+%showdetails(ValkArm);
+%show(ValkArm, config);
